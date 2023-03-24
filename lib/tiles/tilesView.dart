873ffaf70dc22e378b4d6e3855b0661e7be12d04
Steffen Huels <steffen.huels@sap.com>
2023-03-24 14:17:38 +0100

    animation

--- lib/tiles/tilesView.dart
+++ lib/tiles/tilesView.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_template/tiles/tilesWidget.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
 class TilesView extends StatefulWidget {
   const TilesView({super.key, required this.tiles});
@@ -25,9 +26,29 @@ class _TilesViewState extends State<TilesView> {
                 crossAxisCount: 2,
                 crossAxisSpacing: 8,
                 mainAxisSpacing: 8,
-                children: [
-                  for (var tile in widget.tiles) TilesWidget(tile: tile),
-                ]),
+                children: List.generate(widget.tiles.length, (int index) {
+                  return AnimationConfiguration.staggeredGrid(
+                      columnCount: 2,
+                      position: index,
+                      duration: const Duration(milliseconds: 1000),
+                      child: ScaleAnimation(
+                          scale: 0.5,
+                          child: FadeInAnimation(
+                              child: TilesWidget(tile: widget.tiles[index]))));
+                })
+                /*for (var tile in widget.tiles)
+                    AnimationConfiguration.staggeredGrid(
+                      position: widget.tiles.indexOf(tile),
+                      duration: const Duration(milliseconds: 1000),
+                      columnCount: 3,
+                      child: SlideAnimation(
+                        horizontalOffset: 50.0,
+                        child: FadeInAnimation(
+                          child: TilesWidget(tile: tile),
+                        ),
+                      ),
+                    )*/
+                ),
           ]),
         ));
   }
